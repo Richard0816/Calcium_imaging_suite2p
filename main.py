04f231780801b6e83f28e59a2ad0fe0fbfe68fb2
Cembrowski Lab <richardjiang2004@gmail.com>
2025-10-29 13:18:38 -0700

    going to rerun some of the samples on suite2p with new spatial_hp_detect and threshold_scaling currently the one that looks the best is 24, 0.88 respectively, but it is still missing a whole bunch of rois. Will do some further optimization. Also discovered when the background is super noisy all my cells with relatively low number of activity are falsly identified as not cells, maybe look into adding the frequency of activity as one of the cell detection parameters

--- main.py
+++ main.py
@@ -35,7 +35,8 @@ def run_suite2p_on_folder(folder_name: str, addon_vals: list) -> None:
     # Changing the ops file
     change_tau_according_to_GCaMP(ops, tau_vals, "human_SLE_2p_meta.csv", folder_name.split("\\")[-1])
     ops["batch_size"] = utils.change_batch_according_to_free_ram() # can delete if you are happy with the batch_size defined in the ops.npy
-
+    ops["spatial_hp_detect"] = 24.0
+    ops["threshold_scaling"] = 0.88
     # defining the folder
     db = {
         'data_path': [folder_name]
@@ -63,4 +64,17 @@ def run():
 
 
 if __name__ == '__main__':
-    utils.log("suite2p_raw_output.log", run)
+    # taken from suite2p documentation https://suite2p.readthedocs.io/en/latest/settings.html
+    tau_vals = {
+        "6f": 0.7,
+        "6m": 1.0,
+        "6s": 1.3,
+        "8m": 0.137  # empirically defined
+    }
+
+    path_to_ops = "E:\\suite2p_2p_ops_240621.npy"
+
+    ops = np.load(path_to_ops, allow_pickle=True).item()
+
+    run_suite2p_on_folder(r'D:\2024-11-20_00003', [ops, tau_vals])
+    #utils.log("suite2p_raw_output.log", run)
